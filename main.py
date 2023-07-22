d8be187de25644bf54c49c6cbbf0620fa3f97c02
bwindhya <bwindhya@gmail.com>
2023-07-22 20:47:44 +0800

    Add comment in print command

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_halo(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi World, my name is {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
